4515e30aea22ae2c26470fb87e58b2a2e206c241
florianmarsch <florianmarsch@192.168.0.143>
2017-08-23 18:06:38 +0200

    initial

---
+++ worker.js
@@ -0,0 +1,27 @@
+var redis = require("redis");
+
+var publisher  = redis.createClient(process.env.REDIS_URL);
+
+var  minutely =  parseInt((process.env.POLL_TIME || 60*1*1000));
+var hourly = minutely*60;
+var dayly = hourly*24;
+
+	var pollTheMinute= function() {
+		var date = new Date();
+		publisher.publish("minutely", JSON.stringify(date) );
+		if(date.getMinutes() === 0){
+			publisher.publish("hourly", JSON.stringify(date) );
+		}
+		if(date.getHours() === 0){
+			publisher.publish("dayly", JSON.stringify(date) );
+		}
+	};
+
+	setInterval(pollTheMinute, minutely);
+
+	
+	
+		
+	console.log('Node worker is running, start at '+JSON.stringify(new Date()));
+	publisher.publish("debug", 'Node worker is running');
+
